c09f62baf8367a5b119551cf393e8769b5a5c9da
Joe <48526917+jblsp@users.noreply.github.com>
2024-11-20 15:38:06 -0500

    Big telescope config update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,17 +11,41 @@ return {
 				return vim.fn.executable("make") == 1
 			end,
 		},
+		"nvim-telescope/telescope-frecency.nvim",
 		"nvim-telescope/telescope-ui-select.nvim",
+		"nvim-telescope/telescope-live-grep-args.nvim",
+		"nvim-telescope/telescope-symbols.nvim",
 	},
 
 	opts = {
 		defaults = {
 			mappings = {
 				n = {
-					["q"] = function(buf)
-						require("telescope.actions").close(buf)
-					end,
+					["q"] = "close",
 				},
+			},
+			borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+		},
+		pickers = {
+			buffers = {
+				initial_mode = "normal",
+				sort_mru = true,
+				mappings = {
+					n = {
+						["<c-d>"] = function(args)
+							require("telescope.actions").delete_buffer(args)
+						end,
+						[";"] = "close",
+					},
+					i = {
+						["<c-d>"] = function(args)
+							require("telescope.actions").delete_buffer(args)
+						end,
+					},
+				},
+			},
+			spell_suggest = {
+				initial_mode = "normal",
 			},
 		},
 	},
@@ -43,7 +67,14 @@ return {
 		{
 			"<leader>sf",
 			function()
-				require("telescope.builtin").find_files()
+				require("telescope").extensions.frecency.frecency({ workspace = "CWD" })
+			end,
+			desc = "Search Files in cwd",
+		},
+		{
+			"<leader>sF",
+			function()
+				require("telescope").extensions.frecency.frecency()
 			end,
 			desc = "Search Files",
 		},
@@ -55,7 +86,7 @@ return {
 			desc = "Search Telescope Builtin",
 		},
 		{
-			"<leader>sw",
+			"gW",
 			function()
 				require("telescope.builtin").grep_string()
 			end,
@@ -64,9 +95,16 @@ return {
 		{
 			"<leader>sg",
 			function()
-				require("telescope.builtin").live_grep()
+				require("telescope.builtin").live_grep({
+					attach_mappings = function(_, map)
+						map("n", "<C-a>", function()
+							require("telescope").extensions.live_grep_args.live_grep_args()
+						end)
+						return true
+					end,
+				})
 			end,
-			desc = "Search by Grep",
+			desc = " Grep",
 		},
 		{
 			"<leader>sd",
@@ -80,19 +118,13 @@ return {
 			function()
 				require("telescope.builtin").resume()
 			end,
-			desc = "Search Resume",
+			desc = "Resume Previous Search",
 		},
 		{
-			"<leader>s.",
+			";",
 			function()
-				require("telescope.builtin").oldfiles()
-			end,
-			desc = "Search Recent Files",
-		},
-		{
-			"<leader>sb",
-			function()
-				require("telescope.builtin").buffers()
+				local dropdown = require("telescope.themes").get_dropdown({ previewer = false, results_height = 20 })
+				require("telescope.builtin").buffers(dropdown)
 			end,
 			desc = "Search Buffers",
 		},
@@ -114,43 +146,91 @@ return {
 			desc = "Search Neovim Files",
 		},
 		{
-			"<leader>s/",
+			"<leader>sG",
 			function()
-				require("telescope.builtin").live_grep({
+				local opts = {
 					grep_open_files = true,
 					prompt_title = "Live Grep in Open Files",
+				}
+				opts = vim.tbl_deep_extend("force", opts, {
+					attach_mappings = function(_, map)
+						map("n", "<C-a>", function()
+							opts.prompt_title = opts.prompt_title .. " (args)"
+							require("telescope").extensions.live_grep_args.live_grep_args(opts)
+						end)
+
+						return true
+					end,
 				})
+				require("telescope.builtin").live_grep(opts)
 			end,
 			desc = "Live Grep in Open Files",
 		},
 		{
 			"z=",
 			function()
-				require("telescope.builtin").spell_suggest({})
+				local dropdown = require("telescope.themes").get_dropdown({
+					previewer = false,
+				})
+				require("telescope.builtin").spell_suggest(dropdown)
 			end,
 			desc = "Spelling suggestions",
+		},
+		{
+			"<C-Space>e",
+			function()
+				require("telescope.builtin").symbols({ sources = { "emoji" } })
+			end,
+			mode = { "i", "n" },
+			desc = "Insert emoji",
+		},
+		{
+			"<C-Space>m",
+			function()
+				require("telescope.builtin").symbols({ sources = { "math" } })
+			end,
+			mode = { "i", "n" },
+			desc = "Insert math symbol",
+		},
+		{
+			"<C-Space>l",
+			function()
+				require("telescope.builtin").symbols({ sources = { "latex" } })
+			end,
+			mode = { "i", "n" },
+			desc = "Insert latex symbol",
+		},
+		{
+			"<C-Space>n",
+			function()
+				require("telescope.builtin").symbols({ sources = { "nerd" } })
+			end,
+			mode = { "i", "n" },
+			desc = "Insert nerd symbol",
 		},
 	},
 	config = function(_, opts)
 		local extensions = {}
 		local function setup_extension(ext, ext_opts)
-			if opts.extensions == nil then
-				opts.extensions = {}
-			end
+			opts.extensions = opts.extensions or {}
 			if ext_opts then
 				opts.extensions = vim.tbl_deep_extend("force", {}, opts.extensions, { [ext] = ext_opts })
 			end
 			extensions = vim.fn.insert(extensions, ext)
 		end
+		local function setup_telescope()
+			require("telescope").setup(opts)
+			for _, ext in ipairs(extensions) do
+				require("telescope").load_extension(ext)
+			end
+		end
 
-		-- setup extensions here
+		-- setup extensions
 		setup_extension("ui-select", require("telescope.themes").get_dropdown({}))
 		setup_extension("fzf")
+		setup_extension("frecency", { show_filter_column = false })
 
-		require("telescope").setup(opts)
-		for _, ext in ipairs(extensions) do
-			require("telescope").load_extension(ext)
-		end
+		setup_telescope()
 
 		util.lsp_map("n", "gd", function()
 			require("telescope.builtin").lsp_definitions()
