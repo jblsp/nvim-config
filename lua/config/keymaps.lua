0c56f5f1b8cdb87ca279905786e57e2eb7a42c2a
Joe <48526917+jblsp@users.noreply.github.com>
2024-12-08 00:17:23 -0500

    Add source keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,6 +27,11 @@ vim.keymap.set("n", "yc", function()
   vim.fn.setreg("+", vim.fn.getreg('"'))
 end, { desc = "Copy Anon Register to System Clipboard" })
 
+-- source lua
+vim.keymap.set("n", "<leader>X", "<cmd>source %<cr>", { desc = "Source current file" })
+vim.keymap.set("n", "<leader>x", "<cmd>.lua<cr>", { desc = "Source current line" })
+vim.keymap.set("v", "<space>x", "<cmd>lua<cr>", { desc = "Source selection" })
+
 -- LSP mappings
 util.lsp_map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to Declaration" })
 util.lsp_map("n", "<leader>rs", vim.lsp.buf.rename, { desc = "Rename Symbol" })
