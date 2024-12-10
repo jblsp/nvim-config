57289ef660869ad16ebdde2958b19e1119db9a0d
Joe <48526917+jblsp@users.noreply.github.com>
2024-12-10 00:38:04 -0500

    Update keymaps

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -2,3 +2,6 @@ local opt = vim.opt_local
 
 opt.expandtab = true
 opt.shiftwidth = 2
+
+vim.keymap.set("n", "<leader>X", "<cmd>source %<cr>", { desc = "Execute current file" })
+vim.keymap.set("n", "<leader>x", "<cmd>.lua<cr>", { desc = "Execute current line" })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,20 +17,10 @@ vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window 
 vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
--- Bind move to window to <ctrl> hjkl keys
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to the left window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to the down window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to the up window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to the right window" })
-
-vim.keymap.set("n", "yc", function()
+vim.keymap.set("n", "<leader>y", function()
   vim.fn.setreg("+", vim.fn.getreg('"'))
 end, { desc = "Copy Anon Register to System Clipboard" })
-
--- source lua
-vim.keymap.set("n", "<leader>X", "<cmd>source %<cr>", { desc = "Source current file" })
-vim.keymap.set("n", "<leader>x", "<cmd>.lua<cr>", { desc = "Source current line" })
-vim.keymap.set("v", "<space>x", "<cmd>lua<cr>", { desc = "Source selection" })
+vim.keymap.set("n", "yc", "yygccp", { remap = true, desc = "Duplicate line and comment out original" })
 
 -- LSP mappings
 util.lsp_map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to Declaration" })
