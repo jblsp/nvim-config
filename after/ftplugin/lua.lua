102c50be4affbfb1fefd286548c31f6411e3f884
Joe <48526917+jblsp@users.noreply.github.com>
2024-12-13 15:34:43 -0500

    update lua keymaps

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -5,3 +5,4 @@ opt.shiftwidth = 2
 
 vim.keymap.set("n", "<leader>X", "<cmd>source %<cr>", { desc = "Execute current file" })
 vim.keymap.set("n", "<leader>x", "<cmd>.lua<cr>", { desc = "Execute current line" })
+vim.keymap.set("v", "<leader>x", ":lua<CR>", { desc = "Execute current selection" })
