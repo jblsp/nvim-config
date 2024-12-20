32f647a66105f0641fbb191ce92a7202ee483d61
Joe <48526917+jblsp@users.noreply.github.com>
2024-12-20 14:06:09 -0500

    add exclude filetypes to snacks.indent

--- lua/plugins/snacks/indent.lua
+++ lua/plugins/snacks/indent.lua
@@ -12,6 +12,12 @@ return {
       animate = {
         enabled = false,
       },
+      filter = function(buf)
+        local exclude_filetypes = { "text", "markdown" }
+        local ok_filetype =
+          not vim.tbl_contains(exclude_filetypes, vim.api.nvim_get_option_value("filetype", { buf = buf }))
+        return ok_filetype and vim.bo[buf].buftype == ""
+      end,
     },
   },
 }
