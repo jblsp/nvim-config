8bea2694598abb818099ef93d967e6a99432c2e6
Joe <48526917+jblsp@users.noreply.github.com>
2024-12-20 15:59:28 -0500

    Add prettier as html formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -34,6 +34,7 @@ return {
       lua = { "stylua" },
       python = { "black", "isort" },
       markdown = { "prettier" },
+      html = { "prettier" },
     },
     notify_on_error = false,
     format_on_save = function(bufnr)
