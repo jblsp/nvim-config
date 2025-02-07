0c0df277b8635b024261d6627040b0f65dfc0606
Joe <48526917+jblsp@users.noreply.github.com>
2025-02-07 18:25:00 -0500

    Moved treesitter-context plugin file

--- lua/plugins/nvim-treesitter/context.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter-context",
-  event = "VeryLazy",
-  opts = { multiwindow = true, line_numbers = true, max_lines = 4 },
-}
---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  event = "VeryLazy",
+  opts = {
+    multiwindow = true,
+    line_numbers = true,
+    max_lines = 4,
+  },
+}
