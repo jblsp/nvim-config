ee84ce30c15a5b3ae568a08a7386bad5d8020f58
Joe <48526917+jblsp@users.noreply.github.com>
2025-02-19 00:29:55 -0500

    Keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -35,8 +35,18 @@ map("x", "<leader>cp", '"+P', { desc = "Paste from system clipboard" })
 
 -- Comments
 map("n", "gcn", "yygccp", { remap = true, desc = "Duplicate line and comment out original" })
+map("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add comment below" })
+map("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add comment above" })
 
 -- LSP mappings
 map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
 map("n", "<leader>rs", vim.lsp.buf.rename, { desc = "Rename symbol" })
 map({ "n", "x" }, "<leader>lc", vim.lsp.buf.code_action, { desc = "Code action" })
+
+-- (from LazyVim) https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
