fbfd0a4666faeeec4b2c8fa0bc4a9b93e0957fba
jblsp <48526917+jblsp@users.noreply.github.com>
2025-03-18 12:30:12 -0600

    Started snacks.picker config refactor

--- lua/plugins/snacks/picker.lua
+++ lua/plugins/snacks/picker.lua
@@ -1,3 +1,67 @@
+local pickers = {
+  help = function()
+    Snacks.picker.help()
+  end,
+  keymaps = function()
+    Snacks.picker.keymaps({ layout = { preview = false } })
+  end,
+  files = function()
+    Snacks.picker.files()
+  end,
+  grep = function()
+    Snacks.picker.grep()
+  end,
+  grep_buffers = function()
+    Snacks.picker.grep_buffers()
+  end,
+  grep_word = function()
+    Snacks.picker.grep_word()
+  end,
+  buffers = function()
+    Snacks.picker.buffers()
+  end,
+  lines = function()
+    Snacks.picker.lines()
+  end,
+  config_files = function()
+    Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+  end,
+  spelling = function()
+    Snacks.picker.spelling()
+  end,
+  undo = function()
+    Snacks.picker.undo()
+  end,
+  colorschemes = function()
+    Snacks.picker.colorschemes()
+  end,
+  lsp_nav = function()
+    Snacks.picker.lsp_symbols({ layout = { preset = "vscode", preview = "main" } })
+  end,
+  git_branches = function()
+    Snacks.picker.git_branches()
+  end,
+  directories = function()
+    Snacks.picker.pick({
+      title = "Directories",
+      format = "text",
+      finder = function(opts, ctx)
+        local proc_opts = {
+          cmd = "fd",
+          args = { ".", "--type", "directory" },
+        }
+        return require("snacks.picker.source.proc").proc({ opts, proc_opts }, ctx)
+      end,
+      confirm = function(picker, item)
+        picker:close()
+        if item then
+          vim.cmd("e " .. item.text)
+        end
+      end,
+    })
+  end,
+}
+
 return {
   "snacks.nvim",
   opts = {
@@ -13,27 +77,27 @@ return {
   },
   -- stylua: ignore
   keys = {
-    {"<leader>sh", function() Snacks.picker.help() end, desc = "Search help",},
-    {"<leader>sk", function() Snacks.picker.keymaps({ layout = { preview = false } }) end, desc = "Search keymaps",},
-    {"<leader>sf", function() Snacks.picker.files() end, desc = "Search files",},
-    {"<leader>sp", function() Snacks.picker.pickers() end, desc = "Search Snacks.picker sources",},
-    {"<leader>sgg", function() Snacks.picker.grep() end, desc = "Grep",},
-    {"<leader>sgb", function() Snacks.picker.pickers() end, desc = "Grep open buffers",},
-    {"<leader>sw", function() Snacks.picker.grep_word() end, desc = "Grep visual selection or word", mode = { "n", "x" },},
-    {"<leader>bs", function() Snacks.picker.buffers() end, desc = "Search buffers",},
-    {"<leader>/", function() Snacks.picker.lines() end, desc = "Fuzzy find in current buffer",},
-    {"<leader>sn", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Search Neovim config",},
-    {"z=", function() Snacks.picker.spelling() end, desc = "Spelling suggestions",},
-    {"<leader>u", function() Snacks.picker.undo() end, desc = "Undo history",},
+    {"<leader>sp", function() Snacks.picker.pick({}) end, desc = "Search pickers",},
+    {"<leader>sh", pickers.help, desc = "Search help",},
+    {"<leader>sk", pickers.keymaps, desc = "Search keymaps"},
+    {"<leader>sf", pickers.files,  desc = "Search files",},
+    {"<leader>sgg", pickers.grep, desc = "Grep in cwd",},
+    {"<leader>sgb", pickers.grep_buffers, desc = "Grep open buffers",},
+    {"<leader>sw", pickers.grep_word, desc = "Grep visual selection or word", mode = { "n", "x" },},
+    {"<leader>bs", pickers.buffers, desc = "Search buffers",},
+    {"<leader>/", pickers.lines, desc = "Fuzzy find in current buffer",},
+    {"<leader>sn",  pickers.config_files, desc = "Search config files",},
+    {"z=", pickers.spelling,  desc = "Spelling suggestions",},
+    {"<leader>u", pickers.undo, desc = "Undo history",},
     {"<leader>ld", function() Snacks.picker.lsp_definitions() end, desc = "LSP definitions",},
     {"<leader>lr", function() Snacks.picker.lsp_references() end, desc = "LSP references",},
     {"<leader>li", function() Snacks.picker.lsp_implementations() end, desc = "LSP implementations",},
     {"<leader>lD", function() Snacks.picker.lsp_declarations() end, desc = "LSP declarations",},
     {"<leader>lt", function() Snacks.picker.lsp_type_definitions() end, desc = "LSP type definitions",},
     {"<leader>lr", function() Snacks.picker.lsp_references() end, desc = "LSP references",},
-    {"<leader>sc", function() Snacks.picker.colorschemes() end, desc = "Search colorschemes",},
-    {"<leader>ch", function() Snacks.picker.cliphist() end, desc = "Search clipboard history",},
-    {"<leader>ls", function() Snacks.picker.lsp_symbols({ layout = { preset = "vscode", preview = "main" } }) end, desc = "Navigate LSP symbols",},
-    {"<leader>gB", function() Snacks.picker.git_branches() end, desc = "Git Branches",},
+    {"<leader>sc", pickers.colorschemes, desc = "Search colorschemes",},
+    {"<leader>ls", pickers.lsp_nav, desc = "Navigate LSP symbols",},
+    {"<leader>gB", pickers.git_branches, desc = "Git Branches",},
+    {"<leader>sd", pickers.directories, desc = "Search directories" }
   },
 }
