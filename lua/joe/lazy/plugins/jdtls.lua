0b5a8f8f806ec80732ea77df009029749a6fb1f3
jblsp <48526917+jblsp@users.noreply.github.com>
2025-03-27 18:34:07 -0400

    Simplify jdtls config

--- lua/joe/lazy/plugins/jdtls.lua
+++ lua/joe/lazy/plugins/jdtls.lua
@@ -6,65 +6,22 @@ return {
       pattern = "java",
       group = vim.api.nvim_create_augroup("jdtls", { clear = true }),
       callback = function()
-        if vim.fn.executable("java") == 0 then
-          vim.notify("Missing JRE, jdtls cannot attach", vim.log.levels.WARN)
-          return
+        local root_dir =
+          vim.fs.root(0, { ".git", "mvnw", "gradlew", "pom.xml", "settings.gradle", "settings.gradle.ks" })
+
+        local proj_args
+        if root_dir then
+          local proj_cache_path = vim.fn.stdpath("cache") .. "/jdtls/" .. vim.fs.basename(root_dir)
+          proj_args = {
+            "-configuration",
+            proj_cache_path .. "/config",
+            "-data",
+            proj_cache_path .. "/data",
+          }
         end
-
-        local cache_path = vim.fn.stdpath("cache") .. "/jdtls"
-        local config_path = cache_path .. "/config"
-        local jdtls_path = vim.fn.fnamemodify(vim.fn.exepath("jdtls"), ":p:h") .. "/../share/java/jdtls"
-
-        -- this should be changed to retrieve the root with the root function from lspconfig
-        local root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew", "pom.xml" })
-        if root_dir == nil or root_dir == vim.env.HOME then
-          root_dir = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
-        end
-
-        local install_path = vim.fs.find(function(name, _)
-          return name:match("org.eclipse.equinox.launcher_%w+")
-        end, { path = jdtls_path .. "/plugins/" })[1]
-
-        local uname = vim.uv.os_uname()
-        if uname.sysname == "Darwin" then
-          os = "_mac"
-        elseif uname.sysname == "Windows_NT" then
-          os = "_win"
-        elseif uname.sysname == "Linux" then
-          os = "_linux"
-        else
-          os = ""
-        end
-
-        -- jdtls wants the config path to be mutable (this fixes jdtls installed by nix)
-        -- TODO: jdtls still breaks on first launch so i think this function is running asynchronously
-        vim.uv.fs_symlink(jdtls_path .. "/config" .. os .. "/config.ini", config_path .. "/config.ini")
-
-        -- should probably serialize the whole dirname to avoid collisions
-        local project_hash = vim.base64.encode(root_dir)
-        local data_path = cache_path .. "/projects/" .. project_hash
 
         require("jdtls").start_or_attach({
-          cmd = {
-            "java",
-            "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-            "-Dosgi.bundles.defaultStartLevel=4",
-            "-Declipse.product=org.eclipse.jdt.ls.core.product",
-            "-Dlog.protocol=true",
-            "-Dlog.level=ALL",
-            "-Xmx1g",
-            "--add-modules=ALL-SYSTEM",
-            "--add-opens",
-            "java.base/java.util=ALL-UNNAMED",
-            "--add-opens",
-            "java.base/java.lang=ALL-UNNAMED",
-            "-jar",
-            install_path,
-            "-configuration",
-            config_path,
-            "-data",
-            data_path,
-          },
+          cmd = vim.list_extend({ vim.fn.exepath("jdtls") }, proj_args),
 
           root_dir = root_dir,
 
@@ -73,10 +30,7 @@ return {
               buffer = bufnr,
               callback = function(args)
                 if args.data.request.method == "textDocument/formatting" then
-                  local ftype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
-                  if ftype == "java" then
-                    require("jdtls").organize_imports()
-                  end
+                  require("jdtls").organize_imports()
                 end
               end,
             })
